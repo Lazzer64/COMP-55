794c1b94f09d7718a92bc00ee0f69c6cbd236262
Alex Pelavin <ajpele@gmail.com>
2016-03-27 15:52:21 -0700

    Implemented getMatches()

--- src/Board.java
+++ src/Board.java
@@ -11,9 +11,10 @@ public class Board {
         Board board = new Board();
         System.out.println("BOARD...");
         System.out.println(board);
-        board.moveTile(new RowCol(0,2), new RowCol(0,0));
-        board.moveTile(new RowCol(0,0), new RowCol(0,3));
-        System.out.println(board); 
+        int[] matches = board.getMatches();
+        System.out.println("Red Matches: "+matches[0]);
+        System.out.println("Green Matches: "+matches[1]);
+        System.out.println("Blue Matches: "+matches[2]);
     }
 
     public Board(){
@@ -29,10 +30,32 @@ public class Board {
     }
 
     public int[] getMatches() {
-        // TODO implement
-        return null;
+
+        int[] numMatches = new int[] {0,0,0};
+        RowCol[][] matches = new RowCol[NUM_COLS+NUM_COLS][];
+
+        for (int row = 0; row < NUM_ROWS; row++) matches[row] = findMatchInRow(row);
+        for (int col = 0; col < NUM_COLS; col++) matches[NUM_ROWS+col] = findMatchInCol(col); 
+        
+        for(int i = 0; i < matches.length; i++){
+            if(matches[i] != null){
+                switch(tileAt(matches[i][0]).getType()){
+                    case RED:
+                        numMatches[0]++;
+                        break;
+                    case GREEN:
+                        numMatches[1]++;
+                        break;
+                    case BLUE:
+                        numMatches[2]++;
+                        break;
+                }
+            }
+        }
+
+        return numMatches;
     }
-    
+
     public void refillBoard() {
         // TODO implement
     } 
@@ -40,15 +63,47 @@ public class Board {
     public String toString(){
         String s = "";
         for (int row = 0; row < NUM_ROWS; row++) {
-               for (int col = 0; col < NUM_COLS; col++) {
-                   s += tiles[row][col].getType().toString().charAt(0) + " ";
-               }
-               s += "\n";
+            for (int col = 0; col < NUM_COLS; col++) {
+                s += tiles[row][col].getType().toString().charAt(0) + " ";
+            }
+            s += "\n";
         } 
         return s;
     }
-	
+
     // Helpers
+
+    private RowCol[] findMatchInRow(int row) {
+        RowCol[] match = new RowCol[NUM_COLS];
+        for(int i = 0; i < NUM_COLS - 2; i++){
+
+            int numMatch = 0; 
+            while((i+numMatch) < NUM_COLS && tiles[row][i].getType() == tiles[row][i+numMatch].getType()){
+                match[numMatch] = tiles[row][i+numMatch].getPosition();
+                numMatch++;
+            }
+
+            if(numMatch >= 3) return match;
+
+        }
+        return null;
+    }
+
+    private RowCol[] findMatchInCol(int col) {
+        RowCol[] match = new RowCol[NUM_ROWS];
+        for(int i = 0; i < NUM_ROWS - 2; i++){
+
+            int numMatch = 0; 
+            while((i+numMatch) < NUM_ROWS && tiles[i][col].getType() == tiles[i+numMatch][col].getType()){
+                match[numMatch] = tiles[i+numMatch][col].getPosition();
+                numMatch++;
+            }
+
+            if(numMatch >= 3) return match;
+
+        }
+        return null;
+    }
 
     private void shiftRow(int row, int amnt) {
         Tile temp;
@@ -69,7 +124,7 @@ public class Board {
         }
     }
 
-private void shiftCol(int col, int amnt) {
+    private void shiftCol(int col, int amnt) {
         Tile temp;
         for(int e = 0; e < Math.abs(amnt); e++){
             if(amnt > 0){
@@ -90,9 +145,9 @@ private void shiftCol(int col, int amnt) {
 
     private void initTiles() {
         for (int row = 0; row < NUM_ROWS; row++) {
-               for (int col = 0; col < NUM_COLS; col++) {
-                    tiles[row][col] = new Tile(new RowCol(row,col), randomType());
-               }
+            for (int col = 0; col < NUM_COLS; col++) {
+                tiles[row][col] = new Tile(new RowCol(row,col), randomType());
+            }
         }
     }
 
@@ -104,6 +159,10 @@ private void shiftCol(int col, int amnt) {
         if(roll == 0) return TileType.RED;
         if(roll == 1) return TileType.BLUE;
         return TileType.GREEN;
+    }
+
+    private Tile tileAt(RowCol pos){
+        return tiles[pos.getX()][pos.getY()];
     }
 
 }
