b7ba9445b7cdfa54660bb62ca66f75e9c1d30b09
Alex Pelavin <ajpele@gmail.com>
2016-03-27 00:25:02 -0700

    Implemented initialization of Board

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,20 @@
+import java.util.Random;
+
 public class Board {
 
-    Tile[][] tiles;
+    public static final int NUM_ROWS = 5;
+    public static final int NUM_COLS = 5;
+
+    Tile[][] tiles = new Tile[NUM_ROWS][NUM_COLS];
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println("BOARD...");
+        System.out.println(board);
+    }
 
     public Board(){
-        // TODO implement
+        initTiles();
     }
 
     public void moveTile(RowCol start, RowCol end) {
@@ -18,5 +29,34 @@ public class Board {
     public void refillBoard() {
         // TODO implement
     } 
+
+    public String toString(){
+        String s = "";
+        for (int row = 0; row < NUM_ROWS; row++) {
+               for (int col = 0; col < NUM_COLS; col++) {
+                   s += tiles[row][col].getType().toString().charAt(0) + " ";
+               }
+               s += "\n";
+        } 
+        return s;
+    }
+
+    private void initTiles() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+               for (int col = 0; col < NUM_COLS; col++) {
+                    tiles[row][col] = new Tile(new RowCol(row,col), randomType());
+               }
+        }
+    }
+
+    private TileType randomType() {
+
+        Random r = new Random();
+        int roll = r.nextInt(3);
+
+        if(roll == 0) return TileType.RED;
+        if(roll == 1) return TileType.BLUE;
+        return TileType.GREEN;
+    }
 
 }
