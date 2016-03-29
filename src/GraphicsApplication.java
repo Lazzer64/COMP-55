113d9c5bbe513efd715383975d98b72440352e06
Alex Pelavin <ajpele@gmail.com>
2016-03-29 11:25:55 -0700

    Added Osvaldo's starter code

--- src/GraphicsApplication.java
+++ src/GraphicsApplication.java
@@ -1,20 +1,78 @@
-import java.awt.event.MouseEvent;
-import java.awt.event.KeyEvent;
+/*
+ * File: GraphicsApplication
+ * -------------------------
+ * File for subclassing off of to generate the entire
+ * GraphicsApplication.  GraphicsApplication is different
+ * than GraphicsPane because the Application is responsible
+ * for switching between different panes.
+ * 
+ * In subclassing GraphicsApplication, think of this as the starting
+ * point for your entire application.  This is what
+ * you can think of as being your initial graphicsprogram.
+ */
 
-public class GraphicsApplication {
+import acm.program.*;
+import java.awt.event.*;
 
-    GraphicsPane CurScreen;    
-
-    public void switchToScreen(GraphicsPane curScreen){}; 
-    public void showContents(){};
-    public void hideContents(){};
-    public void mousePressed(MouseEvent e){};
-    public void mouseReleased(MouseEvent e){};
-    public void mouseClicked(){};
-    public void mouseMoved(){};
-    public void mouseDragged(){};
-    public void keyPressed(KeyEvent e){};
-    public void keyReleased(KeyEvent e){};
-    public void keyTyped(KeyEvent e){};
-
+public abstract class GraphicsApplication extends GraphicsProgram {
+	private GraphicsPane curScreen;
+	
+	/* switchToScreen(newGraphicsPane)
+	 * -------------------------------
+	 * will simply switch from making one pane that was currently
+	 * active, to making another one that is the active class.
+	 */
+	protected void switchToScreen(GraphicsPane newScreen) {
+		if(curScreen != null) {
+			curScreen.hideContents();
+		}
+		newScreen.showContents();
+		curScreen = newScreen;
+	}
+	
+	/*
+	 * These methods just override the basic
+	 * mouse listeners to pass any information that
+	 * was given to the application to a particular screen.
+	 */
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		curScreen.mousePressed(e);
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		curScreen.mouseReleased(e);
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		curScreen.mouseClicked(e);
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		curScreen.mouseDragged(e);
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		curScreen.mouseMoved(e);
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		curScreen.keyPressed(e);
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		curScreen.keyReleased(e);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		curScreen.keyTyped(e);
+	}
 }
--- src/GraphicsPane.java
+++ src/GraphicsPane.java
@@ -1,17 +1,69 @@
+/* File: GraphicsPane.java
+ * -----------------------
+ * Like you did with your own graphics programs, simply
+ * extend from GraphicsPane and implement
+ * as little or as much of the mouse listeners that you need
+ * for your own programs.  Notice however that in this situation
+ * There is no access to the GraphicsProgram window.
+ * Make sure to distinguish between your constructor
+ * and using showContents and hideContents
+ */
+
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.awt.event.KeyEvent; 
 
-interface GraphicsPane {
+public abstract class GraphicsPane implements Interfaceable {
 
-    public void showContents();
-    public void hideContents();
-    public void mousePressed(MouseEvent e);
-    public void mouseReleased(MouseEvent e);
-    public void mouseClicked();
-    public void mouseMoved();
-    public void mouseDragged();
-    public void keyPressed(KeyEvent e);
-    public void keyReleased(KeyEvent e);
-    public void keyTyped(KeyEvent e);
+	@Override
+	public abstract void showContents();
+
+	@Override
+	public abstract void hideContents();
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
 
 }
