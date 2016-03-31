02fd70f9d8facb974111dc9ee3673a2ee375f7d4
Mark <mark_bely@yahoo.com>
2016-03-30 18:31:05 -0700

    Declaring stuff ill be working on/ testing commiting

--- src/Boss.java
+++ src/Boss.java
@@ -1,5 +1,5 @@
 import java.io.File;
-
+//markys territory
 public class Boss extends Unit{
 
     public Boss(int hp, int maxHp, int attack, int defense, File image) { 
--- src/Unit.java
+++ src/Unit.java
@@ -1,5 +1,5 @@
 import java.io.File;
-
+//markys territory
 public abstract class Unit {
 
 	private int hp;
