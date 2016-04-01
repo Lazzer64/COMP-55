b5bc5d06172a11477118cda9ffef692626ebab47
Alex Pelavin <ajpele@gmail.com>
2016-03-31 17:15:15 -0700

    Added functionality so the board can make matches and refill

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,8 @@
 //Alex and Tom's Territory
-
+import java.util.List;
+import java.util.Queue;
+import java.util.TimerTask;
+import java.util.Timer;
 import java.awt.event.MouseEvent;
 import java.awt.Color;
 import acm.graphics.*;
@@ -9,48 +12,49 @@ public class Game extends GraphicsPane{
 
     public static final int NUM_ROWS = 5;
     public static final int NUM_COLS = 5;
-
+    public static final int TILE_MOVE_DELAY = 250; // In milliseconds
     public static final int TILE_SIZE = 40; 
     public static final int BOARD_X = 0;
     public static final int BOARD_Y = 0;
     public static final Color LINE_COLOR = Color.WHITE;
+    public static final Color EMPTY_TILE_COLOR = Color.LIGHT_GRAY;
 
     Main program;
     int score;
     Unit player, enemy;
     Board board;
-    ArrayList<GObject> display;   
+    ArrayList<GObject> boardObjects;   
     RowCol start, end;
 
-	public void mousePressed(MouseEvent e) {
+    public void mousePressed(MouseEvent e) {
         start = getTileAt(e.getX(),e.getY()).getPosition();
     }
-	public void mouseDragged(MouseEvent e) {
+    public void mouseDragged(MouseEvent e) {
         end = getTileAt(e.getX(),e.getY()).getPosition();
         board.moveTile(start,end);
         start = getTileAt(e.getX(),e.getY()).getPosition();
-        hideContents();
-        showContents();
+        updateTiles();
     }
-	public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(MouseEvent e) {
+        makeMatches();
     }
 
     public Game(Main program){
         this.program = program;
         this.score = 0;
-        this.display = new ArrayList<GObject>();
+        this.boardObjects = new ArrayList<GObject>();
         board = new Board(NUM_ROWS,NUM_COLS);
     }
 
     public void showContents(){
         displayBoard();
-        for(GObject o: display){
+        for(GObject o: boardObjects){
             program.add(o);
         }
     }
 
     public void hideContents(){
-        for(GObject o: display){
+        for(GObject o: boardObjects){
             program.remove(o);
         }
     }
@@ -86,6 +90,7 @@ public class Game extends GraphicsPane{
     }
     
     public void displayBoard() {
+        displayBoardBack();
         displayTiles();
         displayGrid();
     }
@@ -98,7 +103,58 @@ public class Game extends GraphicsPane{
         // TODO implement
     }
     
-    // Helpers
+    // Helpers 
+
+    private void matchEffect(List<Match> matches) {
+        // TODO implement
+        for(Match m: matches) System.out.println(m.getType()+" size: "+m.size());
+    }
+
+    private List<Match> makeMatches() {
+        
+        Queue<Match> matches = board.getMatches();
+        List<Match> totalMatches = new ArrayList<Match>(matches);
+        updateTiles();
+
+        new Timer().schedule(new TimerTask(){ // after TILE_MOVE_DELAY milliseconds drops tiles and repaints 
+
+            public void run(){
+
+                board.dropTiles();
+                updateTiles();
+
+                new Timer().schedule(new TimerTask(){ // after TILE_MOVE_DELAY milliseconds refills tiles and repaints 
+                    public void run(){
+
+                        board.refillBoard();
+                        updateTiles();
+
+                        if(!matches.isEmpty()) { // If there are still matches on the board, repeat
+                            totalMatches.addAll(makeMatches());
+                        }
+
+                    }}, TILE_MOVE_DELAY);
+
+            }}, TILE_MOVE_DELAY);
+
+        matchEffect(totalMatches);
+        return totalMatches;
+    }
+
+    private void displayBoardBack(){
+        GRect background = new GRect(BOARD_X,BOARD_Y,TILE_SIZE*NUM_COLS,TILE_SIZE*NUM_ROWS);
+        background.setColor(EMPTY_TILE_COLOR);
+        background.setFilled(true);
+        boardObjects.add(background);
+    }
+
+    private void updateTiles() {
+        for(GObject o: boardObjects) program.remove(o);
+        boardObjects = new ArrayList<GObject>();
+        displayBoard();
+        for(GObject o: boardObjects) program.add(o);
+    }
+
     private Tile getTileAt(int x, int y){
         int row = (int)((y-BOARD_Y)/TILE_SIZE);
         int col = (int)((x-BOARD_X)/TILE_SIZE);
@@ -112,7 +168,7 @@ public class Game extends GraphicsPane{
             int xEnd = BOARD_X + TILE_SIZE*NUM_COLS;
             GLine line = new GLine(xStart, yPos, xEnd, yPos);
             line.setColor(LINE_COLOR);
-            display.add(line);
+            boardObjects.add(line);
         }
         for (int x = 0; x < NUM_COLS+1; x++) {
             int xPos = BOARD_X + (TILE_SIZE)*x;
@@ -120,7 +176,7 @@ public class Game extends GraphicsPane{
             int yEnd = BOARD_Y + TILE_SIZE*NUM_ROWS;
             GLine line = new GLine(xPos, yStart, xPos, yEnd);
             line.setColor(LINE_COLOR);
-            display.add(line);
+            boardObjects.add(line);
         }
     }
 
@@ -134,12 +190,13 @@ public class Game extends GraphicsPane{
     }
 
     private void displayTile(Tile tile){
+        if(tile == null) return;
         int x = BOARD_X + tile.getPosition().getX()*TILE_SIZE;
         int y = BOARD_Y + tile.getPosition().getY()*TILE_SIZE;
         GRect t = new GRect(x, y, TILE_SIZE, TILE_SIZE);
         t.setColor(TileType.getColor(tile.getType()));
         t.setFilled(true);
-		display.add(t);
+		boardObjects.add(t);
     }
     
 }
