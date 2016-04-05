16c461f3157d424732bc5b3eb6dec43aff9d7018
Tom Vo <t_vo16@u.pacific.edu>
2016-04-04 23:45:42 -0700

    Added a compare function to compare scores, working on scores class

--- src/HighscoreList.java
+++ src/HighscoreList.java
@@ -1,14 +1,34 @@
 //Tom's territory 
 //Beginning 4/1/16
 import java.io.File;
+import java.util.ArrayList;
 
 public class HighscoreList {
 
     File scoreList;
     Score[] scores;
+    private int numberScores;
 
+    /*public HighscoreList()
+    {
+    	scoreList = new ArrayList<Score>();
+    }
+    
+    public int getnumScores()
+    {
+    	return numberScores;
+    }
+    
+    public void addScore(Score scoreObj)
+    {
+    	numberScore
+    	
+    	
+    }
+    */
+    
     public void writeScore(Score score) {
-        // TODO implement
+        
     }
     
     public Score[] readScores() {
--- src/Score.java
+++ src/Score.java
@@ -10,7 +10,30 @@ public class Score {
 
     static Score parseScore(String input) {
         // TODO implement
-        return null;
+    	return null;
+    }
+    
+    
+    /**Compares the scores in the list
+     * @param score1
+     * @param score2
+     * @return
+     */
+    public int compare(Score score1, Score score2){
+    	int s1 = score1.getScore();
+    	int s2 = score2.getScore();
+    	
+    	if(s1 > s2){
+    		return -1;
+    	}
+    	
+    	else if (s1 < s2){
+    		return +1;
+    		}
+    	else {
+    		return 0;
+    	}
+     	
     }
     
     // Getters and Setters 
