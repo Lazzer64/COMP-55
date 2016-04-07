bd5f0640ef5ed9783c8cfd2d0a04fa5d8502549e
Mark <mark_bely@yahoo.com>
2016-04-06 23:40:13 -0700

    preset all the values for enemy and made boss extend from it

--- src/Boss.java
+++ src/Boss.java
@@ -1,9 +1,13 @@
 import java.io.File;
 //mark's territory
-public class Boss extends Unit{
+public class Boss extends Enemy{
 
-    public Boss(int hp, int maxHp, int attack, int defense, File image) { 
-        super(hp, maxHp, attack, defense, image);
+    public Boss( File image) { 
+        super(HP, DEFENSE, image);
+        generateAttack(MINATTACK, MAXATTACK);
     }
-    
+    private static final int HP = 150;
+    private static final int DEFENSE = 4;
+    private static final int MINATTACK=1;
+    private static final int MAXATTACK=8;
 }
--- src/Enemy.java
+++ src/Enemy.java
@@ -1,9 +1,23 @@
 import java.io.File;
+import java.util.Random;
 //mark's territory
-public class Enemy extends Unit{
 
-    public Enemy(int hp, int maxHp, int attack, int defense, File image) { 
-        super(hp, maxHp, attack, defense, image);
+public class Enemy extends Unit{
+    public Enemy(int hp, int defense,  File image ){
+    	super(hp, hp, 0, defense, image);
+    }
+    public Enemy( File image) { 
+        super(HP, HP, 0, DEFENSE, image);
+        rng = new Random(System.currentTimeMillis());
+        generateAttack(MINATTACK, MAXATTACK);    }
+    private Random rng;
+    private static final int HP = 100;
+    private static final int DEFENSE = 2;
+    private static final int MINATTACK=1;
+    private static final int MAXATTACK=5;
+    public void generateAttack(int min, int max){
+    	
+    setAttack(min + rng.nextInt(max-min+1));
     }
     
 }
