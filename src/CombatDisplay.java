1f94666080145c6d91a6328b607e66c1ab4d4f4c
Alex Pelavin <ajpele@gmail.com>
2016-04-07 01:13:01 -0700

    Updated game to use the newly implemented unit functions

--- src/CombatDisplay.java
+++ src/CombatDisplay.java
@@ -12,16 +12,18 @@ public class CombatDisplay extends Display{
     }
     
     public void displayCombatField(Unit player, Unit enemy) {
+        clean();
         int x = 50, y = 50;
         displayUnit(x,y,player);
         displayUnit(Main.WINDOW_WIDTH - x - 75,y,enemy);
+        showContents();
     }
     
     public void displayUnit(int x, int y, Unit unit) {
 
         int width = 75, height = 150; // TODO change rectangle to display unit's image
-        int unitHp = 7;// TODO replace with unit.getHp() when implemented;
-        int unitMaxHp = 10;// TODO replace with unit.getMaxHp() when implemented;
+        int unitHp = unit.getHp();
+        int unitMaxHp = unit.getMaxHp();
 
         GRect u = new GRect(x,y,width,height);
 
