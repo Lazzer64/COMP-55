ca593ceaabc6276665bb0f36b07690f49c06fc7c
Mark <mark_bely@yahoo.com>
2016-04-13 17:47:37 -0700

    Added first button to main menu screen

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,11 +1,18 @@
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
+import acm.graphics.GRect;
 
 import java.awt.event.KeyEvent; 
 
 public class MainMenu extends GraphicsPane {
-
+private Main program;
+private GRect rect;
+	public  MainMenu(Main app) {
+		program = app;
+		rect = new GRect(Main.WINDOW_WIDTH/2-25, Main.WINDOW_HEIGHT/2-75/2, 50, 75);
+		rect.setFilled(true);
+	}
 	 public void initBackground() {
 		 	//initialize the main menu background and set size
 	    	GImage background = new GImage("SpriteSheets/mainmenuBackground.gif");
@@ -14,6 +21,7 @@ public class MainMenu extends GraphicsPane {
 	 
     public void showContents() {
     	 initBackground();
+    program.add(rect);
     }
     
     public void hideContents() {
