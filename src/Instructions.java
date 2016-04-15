6d9f2d35c377f3353732546a02ac53d23375c4eb
Mark <mark_bely@yahoo.com>
2016-04-14 22:53:50 -0700

    Filled in the Instructions function in Instructions.

--- src/Instructions.java
+++ src/Instructions.java
@@ -1,9 +1,36 @@
 import java.awt.event.MouseEvent;
+
 import java.awt.event.KeyEvent; 
 
+import acm.graphics.*;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent
+;
 public class Instructions extends GraphicsPane {
 
-    public void showContents() {
+    private Main program;
+    private GRect returnbtn;
+    private GLabel returnlab;
+    private GLabel instructions;
+    
+    public static final int WIDTH = 200;
+    public static final int HEIGHT = 50;
+    public static final int xPos = Main.WINDOW_WIDTH/2-WIDTH/2;
+    public static final int OFFSET = 75;
+    
+    public Instructions(Main app){
+    	program =app;
+    	instructions = new GLabel("INSTRUCTIONS", xPos, 100);
+    	instructions.setColor(Color.BLUE);
+    	instructions.setFont("Arial-30");
+    	returnbtn = new GRect(Main.WINDOW_WIDTH/2-WIDTH/2, OFFSET*6, WIDTH, HEIGHT);
+		returnbtn.setFilled(false);
+		returnlab = new GLabel("<= RETURN", Main.WINDOW_WIDTH/2-WIDTH/4, OFFSET*6+75/2);
+		returnlab.setColor(Color.RED);
+		returnlab.setFont("Arial-20");
+    }
+	public void showContents() {
         // TODO implement
     }
     
@@ -19,7 +46,7 @@ public class Instructions extends GraphicsPane {
         // TODO implement
     }
     
-    public void mouseClicked() {
+    public void mouseClicked(MouseEvent e) {
         // TODO implement
     }
     
