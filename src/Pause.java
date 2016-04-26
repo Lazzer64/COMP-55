ffe53fd9bb11293c194216e83d80d1ae554e8602
Mark <mark_bely@yahoo.com>
2016-04-25 18:59:38 -0700

    Set up constructors for pause screen

--- src/Pause.java
+++ src/Pause.java
@@ -1,8 +1,29 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.*;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 public class Pause extends GraphicsPane {
+private Main program;
+private GImage pausepic;
+private GImage background;
+private GImage backtogamepic;
+private GImage instructionspic;
+private GImage mainmenupic;
 
-	public Pause() {
+public static final int WIDTH = 200;
+public static final int HEIGHT = 50;
+public static final int OFFSET = 75;
+public static final int xPos = Main.WINDOW_WIDTH/2-WIDTH/2;
+
+private Game game;
+private Instructions instructions;
+private MainMenu mainmenu;
+	public Pause(Main app) {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
