7f2837997ca7c173ecd750a2a3528999b4e12cac
Mark <mark_bely@yahoo.com>
2016-04-25 18:45:28 -0700

    Added Pause Class

---
+++ src/Pause.java
@@ -0,0 +1,20 @@
+
+public class Pause extends GraphicsPane {
+
+	public Pause() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void showContents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hideContents() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
